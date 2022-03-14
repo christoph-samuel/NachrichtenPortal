4f287463cb7e3c27da7b2130b0ff86f0ff5bda63
Christoph Samuel <chsamuel@gmx.at>
2022-03-14 15:21:34 +0100

    Initial Commit with custom Button-Component (from Bit)

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Button as BitButton} from '@onpoint/workshop6.ui.button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <BitButton text="hello from Button" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
